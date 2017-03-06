f69c1ed816a67fe3114195ad474bf035bd1efcb4
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-03-06 14:04:22 +0100

    Finished Lecture 62

--- Lecture59/after/js/script.js
+++ Lecture59/after/js/script.js
@@ -1,5 +1,5 @@
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
-
+  
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
